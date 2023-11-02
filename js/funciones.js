41b6550b89cbd29888f3efafd9d10ae06c4b1300
Jennifer Valor <profe.jennifer.valor@gmail.com>
2023-11-02 11:15:33 -0300

    agregamos una funcion en forma de flecha

--- js/funciones.js
+++ js/funciones.js
@@ -2,10 +2,25 @@
 function saludar(){
     document.write('<p>Hola mundo');
 } 
-
+//funciones que le pongo un valor
 function nuevoSaludo(nombreIngresado, apellidoIngresado){
     document.write(`<p> Hola ${nombreIngresado}, ${apellidoIngresado} que tengas un buen día </p>`);
 } 
+
+//funciones que retoman un valor
+//function conversionAPesos(dolares){
+//    const precioFinal = dolares *1000;
+//    console.log(precioFinal);
+//    return precioFinal; // el return es la ultima linea de codigo, lo que viene despues no lo lee
+//}
+//arrow functions
+//const conversionAPesos = (dolares) =>{
+//    const precioFinal = dolares *1000;
+//    console.log(precioFinal);
+//    return precioFinal;
+//}
+
+const conversionAPesos = (dolares) =>dolares *1000;//se puede realizar la operacion en una sola linea y si es una sola linea de codigo 
 
 //invocar a una funcion 
 saludar();//funcion sin parametros
@@ -20,3 +35,7 @@ nuevoSaludo('Luciano', 'Cabello')
 nuevoSaludo('Sebastian');
 
 nuevoSaludo();
+
+let precioProducto = conversionAPesos(35);
+document.write(`<p>El precio dle producto de prueba es: $${precioProducto}</p>`);
+document.write(`<p>El precio dle producto de prueba es: $${conversionAPesos(40)}</p>`);
